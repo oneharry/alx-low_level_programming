63e6ad1127fb5b9cbae292986a0c6c22dada0cef
oneharry <harryofficialchidi@yahoo.com>
2022-09-08 11:03:24 -0700

    added sice C program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	long long int c;
 	float d;
 
-	printf("size of a char: %lu byte(s)\n", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", sizeof(e));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
