82136a8c31c7c7492e68391d2d331a80b93fa52e
oneharry <harryofficialchidi@yahoo.com>
2022-09-08 08:28:58 -0700

    added c program that prints to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ * Output - Output string 
+ * Return - Returns 0 (Success) 
+ */
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
+}
+  
