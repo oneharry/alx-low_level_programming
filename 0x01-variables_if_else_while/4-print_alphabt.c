ebca63f65647b3bd8b31c756d85a03f315b3658a
oneharry <harryofficialchidi@yahoo.com>
2022-09-09 04:51:16 -0700

    added program files

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if ((x != 'q') && (x != 'e'))
+			putchar(x);
+	}
+		putchar('\n');
+		return (0);
+}
