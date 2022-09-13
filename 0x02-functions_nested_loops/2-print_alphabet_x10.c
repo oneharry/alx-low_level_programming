6e38bb62adf936433fbb5a44f13c6daec41c4591
oneharry <harryofficialchidi@yahoo.com>
2022-09-13 10:04:37 -0700

    made changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * print_alphabet_x10 - Prints letter of alphabet in 10 places
+ * Return: void
  */
-void print_alphabet(void);
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 
@@ -18,5 +17,4 @@ int main(void)
 		putchar('\n');
 		i++;
 	}
-	return (0);
 }
