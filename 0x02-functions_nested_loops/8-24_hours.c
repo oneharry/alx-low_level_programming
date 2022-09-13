1b9124cec9580d4abad9f2bbf1a601c8e7c3a087
oneharry <harryofficialchidi@yahoo.com>
2022-09-13 12:00:43 -0700

    added bauers counter

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 	int first = 0;
 	int last = 0;
 
-	while (first <= 23)
+	for (first = 0; first <= 23; first++)
 	{
-		while (last <= 59)
+		for (last = 0; last <= 59; last++)
 		{
 			_putchar((first / 10) + '0');
 			_putchar((first % 10) + '0');
@@ -19,9 +19,7 @@ void jack_bauer(void)
 			_putchar((last / 10) + '0');
 			_putchar((last % 10) + '0');
 			_putchar('\n');
-			last++;
 		}
-		first++;
 	}
 }
 
