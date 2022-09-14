c335af2077125c6ad75e69944090250a27e0d28c
oneharry <harryofficialchidi@yahoo.com>
2022-09-14 21:30:09 +0000

    added first debug file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry function, prints integer
+ * Return: returns 0 (success)
+ */
+int mian(void)
+{
+	int i;
+
+	i = 98;
+	printf("%d", positive_or_negative(i));
+
+	return(0);
+}
