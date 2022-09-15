0e62e99f1073251f80169c46fa7a1a99a7b2b1ac
oneharry <harryofficialchidi@yahoo.com>
2022-09-15 12:12:34 -0700

    added more numbers function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,10 @@ void more_numbers(void)
 	{
 		for (x = 0; x < 14; x++)
 		{
-			if (x <= 9)
-				_putchar(x + '0');
-			else
-			{
+			if (x > 9)
 				_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
-			}
+
+			_putchar((x % 10) + '0');
 		}
 
 		_putchar('\n');
