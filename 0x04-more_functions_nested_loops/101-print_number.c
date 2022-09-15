b0a55f63f41078704980cc33e53df7ba042aa708
oneharry <harryofficialchidi@yahoo.com>
2022-09-15 14:37:10 -0700

    added number function

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_number - Prints a square followed by a newline
+ * @size: size argument
+ * Return: void
+ */
+
+void print_triangle(int n)
+{
+	_putchar('\n');
+}
