e4634de817ca3638beea25aac66dbe741c762443
oneharry <harryofficialchidi@yahoo.com>
2022-09-15 12:49:14 -0700

    added print diagonal function

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,15 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (x = 0; x < n; n++)
+		for (x = 0; x < n; x++)
 		{
 			for (y = 0; y < x; y++)
 				_putchar(' ');
-			_putchar('_');
+			_putchar('\\');
+
+			if (x == n - 1)
+				continue;
+			_putchar('\n');
 		}
 	}
 	_putchar('\n');
