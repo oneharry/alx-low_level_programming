3ec9ab9d450eb42fb67f906eb7b78aaa96815e9c
oneharry <harryofficialchidi@yahoo.com>
2022-09-15 14:28:24 -0700

    added print prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,24 +8,7 @@
 
 int main(void)
 {
-	long x = 612852475143;
-	long y;
-
-	while (y < (x / 2))
-	{
-		if ((x % 2) == 0)
-		{
-			x = x / 2;
-			continue
-		}
-
-		for (y = 3; y < (x / 2); y += 2)
-		{
-			if ((x % y) == 0)
-				x /= x
-		}
-	}
-	printf("%ld\n", x);
+    
 
 	return (0);
 }
