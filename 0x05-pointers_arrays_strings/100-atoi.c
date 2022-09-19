23e38dc7006fd741f8e9465a6c260b963a84b688
oneharry <harryofficialchidi@yahoo.com>
2022-09-19 13:06:40 -0700

    added atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - Copy string to another pointer
+ * @s: pointer argument
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int x, num;
+
+	for (x = 0; src[x]; x++)
+		num *= src[x] - '0';
+	return (num);
+}
