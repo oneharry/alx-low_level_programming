a0827eead5a8f8ad69f198d8c5d84582eef5d7b8
oneharry <harryofficialchidi@yahoo.com>
2022-09-19 10:57:41 -0700

    added function printing everyother char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - Prints every other string array element
+ * @s:string argument
+ * Return: void
+ */
+
+void puts2(char *s)
+{
+	int len = _strlen(s);
+	int x = 0;
+
+	for (x = 0; x < len; x + 2)
+	{
+		_putchar(s[i]);
+	}
+}
+/**
+ * _strlen - returns length os string
+ * @s: string argument
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != 0)
+		counter++;
+	return (counter);
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -8,34 +8,29 @@
 
 int main(void)
 {
-	char s[10] = "My school";
-
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
+	char *str;
+	str = "0123456789";
+	
+	puts2(str);
 	return (0);
 }
-void rev_string(char *s)
+void puts2(char *s)
 {
 	char hold;
 	int len = (_strlen(s) - 1);
 	int i = 0;
 
-	while (i < len)
+	for (i = 0; i < len; i + 2)
 	{
-		hold = s[i];
-		s[i] = s[len];
-		s[len] = hold;
-		len--;
-		i++;
-	      len);
+		putchar('H');
+		putchar(s[i]);
 	}
 	putchar('\n');
 }
-int _strlen(char s[])
+int _strlen(char *s)
 {
 	int x = 0;
-	while (s[x] != 0)
+	while (s[x])
 	{
 		x++;
 	}
