89b31e663711064d0aa13f15ba7a87bdad96f6f4
oneharry <harryofficialchidi@yahoo.com>
2022-09-19 12:25:28 -0700

    added function printing n times of element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Print n number of elements of string array element
+ * @a: string argument
+ * @n: number of elements to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int len = _strlen(a);
+
+	if (len >= n)
+	{
+		int x;
+
+		for (x = 0; x < n; x++)
+		{
+			_putchar(a[x]);
+		}
+		_putchar('\n');
+	}
+}
+/**
+ * _strlen - returns length os string
+ * @s: string argument
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != 0)
+		counter++;
+	return (counter);
+}
