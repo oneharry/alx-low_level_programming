614346739f4368ba0e315042152a5c68ece7eaef
oneharry <harryofficialchidi@yahoo.com>
2022-09-19 08:51:13 -0700

    added function print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts - Prints the string passed in
+ * print_rev - Prints the string passed in reverse form
  * @s:string argument
  * Return: void
  */
 
-void _puts(char *s)
+void print_rev(char *s)
 {
-	int x = 0;
+	int len = _strlen(s);
+	int x;
 
-	for (x = 0; s[x] != '\0'; x++)
+	for (x = len - 1; x >=0; x--)
 		_putchar(s[x]);
 	_putchar('\n');
+}
+int _strlen(char *s)
+{
+	int counter = 0;
+	while (s[counter] != 0)
+		counter++;
+	return (counter);
 }
