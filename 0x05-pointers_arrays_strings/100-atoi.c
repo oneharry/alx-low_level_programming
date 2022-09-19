edfad94ab173d32db862da7e964a7101fc70e469
oneharry <harryofficialchidi@yahoo.com>
2022-09-19 15:31:46 -0700

    added atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,13 +11,14 @@ int _atoi(char *s)
 	int x = 1;
 	unsigned int y = 0;
 
-	do {
-		if (*s == '-')
-			x *= -1;
+	while (*s)
+	{
+		if (*s < 0)
+			y *= -1;
 		else if (*s >= '0' && *s <= '9')
 			y = (y * 10) + (*s - '0');
 		else if (y > 0)
 			break;
-	} while (*s++);
+	}
 	return (y);
 }
