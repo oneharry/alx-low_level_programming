db11d31c9b3944b324719d62dda2b88bd0a55eb2
oneharry <harryofficialchidi@yahoo.com>
2022-09-21 16:06:00 -0700

    added more function files

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -7,30 +7,12 @@
  */
 int main(void)
 {
-	char s1[98];
 	char *ptr;
-	int i;
 	char s2[] = "First, solve the problem. Then, write the code\n";
 
-	for (i = 0; i < 98 - 1; i++)
-		s1[i] = '*';
-	s1[i] = '\0';
-	printf("%s\n", s1);
-	ptr = _strncpy(s1, s2, 5);
-	printf("%s\n", s1);
-	printf("%s\n", ptr);
-	ptr = _strncpy(s1, s2, 90);
-	printf("%s", s1);
+	ptr = cap_string(s2);
 	printf("%s", ptr);
-	for (i = 0; i < 98; i++)
-	{
-		if (i % 10)
-			printf(" ");
-		if (!(1 % 10) && i)
-			printf("\n");
-		printf("0x%02x", s1[i]);
-	}
-	printf("\n");
+	printf("%s", ptr);
 	return (0);
 
 }
