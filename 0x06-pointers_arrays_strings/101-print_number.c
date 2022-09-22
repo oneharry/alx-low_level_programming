4eca618315789c7c15cff2b3278613885a19dee6
oneharry <harryofficialchidi@yahoo.com>
2022-09-22 12:38:25 -0700

    added more files

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,18 +2,18 @@
 /**
  * print_number - print numbers only
  * @n: integer
- * Return: pointer to the encoded string
+ * Return: void
  */
 void print_number(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
 	if (n == 0)
 		_putchar('0');
-	if(n/10)
+	if (n / 10)
 		print_number(n/10);
 	_putchar(n % 10 + '0');
 }
