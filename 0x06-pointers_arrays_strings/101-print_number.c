1e21124cc2f52bb923fbdc98f3abf31bf03cd0d8
oneharry <harryofficialchidi@yahoo.com>
2022-09-22 15:22:34 -0700

    added some more files

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,13 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
 	if (n < 0)
 	{
 		putchar('-');
-		x = -x;
+		n = -n;
 	}
 
-	if ((x / 10))
-		print_number(x/10);
-	putchar((x % 10) + '0');
+	if ((n / 10))
+		print_number(n / 10);
+	putchar((n % 10) + '0');
 }
