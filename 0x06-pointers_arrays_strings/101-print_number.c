514b107142c82174ae4c3a9454c83c038b582cc1
oneharry <harryofficialchidi@yahoo.com>
2022-09-22 14:42:29 -0700

    added more function files

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,9 +12,9 @@ void print_number(int n)
 		_putchar('-');
 		x = -x;
 	}
-	if (n == 0)
+	if(n == 0)
 		_putchar('0');
-	if (n / 10)
+	if ((n / 10) > 0)
 		print_number(n/10);
 	_putchar((n % 10) + '0');
 }
