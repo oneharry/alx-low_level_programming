7033b67240842bb17f56e3f86470d72152fef28e
oneharry <harryofficialchidi@yahoo.com>
2022-09-22 17:00:18 -0700

    added changes to file

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -13,7 +13,10 @@ int main(void)
 	print_number(402);
 	putchar('\n');
 	print_number(1024);
+	putchar('\n');
 	print_number(0);
+	putchar('\n');
 	print_number(-98);
+	putchar('\n');
 	return (0);
 }
