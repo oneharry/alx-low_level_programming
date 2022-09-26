6b01cdac1a5239b1e52f54e47665b1a02685821b
oneharry <harryofficialchidi@yahoo.com>
2022-09-26 11:09:38 -0700

    added strbrk function

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,14 +5,15 @@
   * @accept: pointer the string that is evaluated
   * Return: a pointer to the first occurence of c
 */
-unsigned int _strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	for (int x = 0; *s; x++, s++)
+	int y;
+	for (; *s; s++)
 	{
-		for (int y = 0; *accept; y++, accept++)
+		for (y = 0; *accept; y++, accept++)
 		{
-			if (*(accept + y) == *(s + x))
-				return (*(s + x));
+			if (*(accept + y) == *s)
+				return (s);
 		}
 	}
 	return (NULL);
