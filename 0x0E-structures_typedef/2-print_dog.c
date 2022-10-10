0aac0d81fd3fafcfaff67757789447b6452bbcc8
oneharry <harryofficialchidi@yahoo.com>
2022-10-10 13:43:36 -0700

    added function files

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+
+/**
+  * print_dog - print the dog
+  * @d: instance of dod
+  * Return: void
+  */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: nil\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0)
+		printf("Age: nil\n");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: nil\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+/**
+  * new_dog - new dog created
+  * @name: name of new dog
+  * @age: age of new dog
+  * @owner: owner of new dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
