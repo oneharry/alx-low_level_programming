7b367053c85cc7c1d66ec5528114839eba41c2df
oneharry <harryofficialchidi@yahoo.com>
2022-10-12 10:21:15 -0700

    added iterator func

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,8 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	unsigned int x;
 
+	if (array == NULL || action == NULL)
+		return;
 	for (x = 0; x < size; x++)
 	{
 		action(array[x]);
