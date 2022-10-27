31927f68d3c1852220d9c9ab2874d78facdb4bc1
oneharry <harryofficialchidi@yahoo.com>
2022-10-27 11:39:28 -0700

    added funtion task 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_binary - prints binary representation of a num
+  * @n: inter argument
+  * Return: binary representation of the number
+  */
+void print_binary(unsigned long int n)
+{
+	 int temp = n;
+
+	if (temp != 0)
+	{
+		print_binary(temp >> 1);
+		if (temp & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+	else
+		return;
+}
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * _putchar - prints char
+  * @c: char to be printed
+  ^ Retrun: Returned 
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
