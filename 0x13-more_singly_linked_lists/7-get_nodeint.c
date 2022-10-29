3216b94ff260b48979f3dd1121af596525d4e16e
oneharry <harryofficialchidi@yahoo.com>
2022-10-29 21:38:36 +0000

    added function corrections

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,40 +8,14 @@
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *new;
 	unsigned int i = 0;
-	int num = index;
 
 	if (head == NULL)
 		return (NULL);
-	new = (listint_t *)malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	if (index == 1)
+	while (i < index)
 	{
-		new = head;
-		return (new);
-	}
-	if (num < 0)
-	{
-		while (head)
-		{
-			if ((i + num) == 0)
-			{
-				new = head;
-				return (new);
-			}
-			i++;
-		}
-	}
-	while (head)
-	{
-		if (i++ == index)
-		{
-			new = head;
-			return (new);
-		}
 		head = head->next;
+		i++;
 	}
-	return (NULL);
+	return (head);
 }
