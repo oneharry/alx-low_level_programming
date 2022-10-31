5d56ecdd2a8c393e5af58e0c1751d4a6ec48e93f
oneharry <harryofficialchidi@yahoo.com>
2022-10-31 12:34:36 -0700

    added funcs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,9 @@ int create_file(const char *filename, char *text_content)
 		txt++;
 	}
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT |  O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	w = write(fd, text_content, len);
