b2d10c5a1f6bb1cbdfa880d8ab5faa0843ffb7ff
oneharry <harryofficialchidi@yahoo.com>
2022-11-01 02:37:21 -0700

    fixing bug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	w = write(fd, text_content, len);
-	if (w == -1 || w != len)
+	if (w == -1)
 		return (-1);
 	close(fd);
 	return (1);
