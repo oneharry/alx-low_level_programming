ddc6a2478fa6d4a33b74d0936f7587ee80421f44
oneharry <harryofficialchidi@yahoo.com>
2023-04-11 22:13:33 +0000

    Implemeted linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/*
+   * linear_search - Implementatio of linear sesrch algorithm
+   * @array: pointer to the start of an array
+   * @size: number of element
+   * @value: Searched value
+   * Return: index of the first instance of value, -1 otherwise
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	if (array == NULL || *array == NULL)
+		return (-1);
+	while (array[i++])
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
