55f19a547994053f2711ad39c7cf6b39bb1a82ad
oneharry <harryofficialchidi@yahoo.com>
2023-04-11 22:52:47 +0000

    binary search file added

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Implementatio of binary sesrch algorithm
+ * @array: pointer to the start of an array
+ * @size: number of element
+ * @value: Searched value
+ * Return: index of the first instance of value, -1 otherwise
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (array[i] && i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
