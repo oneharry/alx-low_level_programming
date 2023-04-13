308c6afbcd28c93233a473252a0336e83208a91b
oneharry <harryofficialchidi@yahoo.com>
2023-04-13 22:22:26 +0000

    advanced binary

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+#include <stdlib.h>
+
+void print_array(int *array, int start, int stop);
+
+/**
+ * advanced_binary - Implementatio of binary sesrch algorithm
+ * @array: pointer to the start of an array
+ * @size: number of element
+ * @value: Searched value
+ * Return: index of the first instance of value, -1 otherwise
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int mid = size / 2;
+	int start = 0, stop = size - 1;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	while (stop >= start)
+	{
+		print_array(array, start, stop);
+		mid = (start + stop) / 2;
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		if (array[mid] > value)
+		{
+			stop = mid - 1;
+		} else if (array[mid] < value)
+		{
+			start = mid + 1;
+		}
+
+	}
+	return (-1);
+}
+
+/**
+  * print_array - Prints array of integers
+  * @array: pointer to the array
+  * @start: starting index of array
+  * @stop: Last index of the array
+  * Return: void
+  */
+
+void print_array(int *array, int start, int stop)
+{
+	int i = start, x = 0;
+
+	printf("Searching in array: ");
+	while (i <= stop)
+	{
+		if (x > 0)
+			printf(", ");
+		x = 1;
+		printf("%d", array[i]);
+		i++;
+	}
+	printf("\n");
+}
