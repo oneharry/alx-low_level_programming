56d6ee950e4f416758d8c18ebe7b990f09cfc575
oneharry <harryofficialchidi@yahoo.com>
2023-04-14 22:22:01 +0000

    exponential

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -2,7 +2,56 @@
 #include <stdlib.h>
 #include <math.h>
 
-void print_array(int *array, int start, int stop);
+/**
+  * print_array - Print an array of integers
+  * @array: pointer to an array
+  * @l: start position
+  * @r: end position
+  * Return: void
+  */
+
+void print_array(int *array, int l, int r)
+{
+	int i, flag = 0;
+
+	printf("Searching in array: ");
+	for (i = l; i < r; i++)
+	{
+		if (flag)
+			printf(", ");
+		flag = 1;
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
+
+/**
+  * bin_search - Implementation of binary search
+  * @array: pointer to the array
+  * @l: start point of the array
+  * @r: stop point of the array
+  * @value: integer value to be searched
+  * Return: index of the value
+  */
+int bin_search(int *array, int l, int r, int value)
+{
+	int mid;
+
+	while (r > l)
+	{
+		mid = (l + r) / 2;
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+			l = mid + 1;
+		else
+			r = mid - 1;
+		print_array(array, l, r + 1);
+	}
+	return (-1);
+}
 
 /**
  * exponential_search - Implementatio of exponential sesrch algorithm
@@ -15,6 +64,7 @@ void print_array(int *array, int start, int stop);
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t bound = 1;
+	int temp;
 
 	if (array == NULL || size == 0)
 		return (-1);
@@ -25,8 +75,9 @@ int exponential_search(int *array, size_t size, int value)
 		bound *= 2;
 	}
 
-	r = bound < size ? bound : size - 1;
-	printf("Value found between indexes [%ld] and [%ld]\n", bound / 2, r);
+	temp = bound < size ? bound : size - 1;
+	printf("Value found between indexes [%ld] and [%d]\n", bound / 2,
+			temp);
 
-	return (binary_search(array, bound / 2, temp, value));
+	return (bin_search(array, (bound / 2), temp, value));
 }
